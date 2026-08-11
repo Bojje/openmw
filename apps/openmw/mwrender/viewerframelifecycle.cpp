88c875031b361d159aad3b8f6e325aa36d7180ce
Mathias <m-boisen@hotmail.com>
2026-08-12 00:15:28 +0200

    Remove unused frame sync callback bridge

--- apps/openmw/mwrender/viewerframelifecycle.cpp
+++ apps/openmw/mwrender/viewerframelifecycle.cpp
@@ -1,15 +1,11 @@
 #include "viewerframelifecycle.hpp"
 
-#include <utility>
-
 #include <osgViewer/Viewer>
 
 namespace MWRender
 {
-    ViewerFrameLifecycle::ViewerFrameLifecycle(osgViewer::Viewer& viewer,
-        std::function<void(Render::SceneData&)> synchronizeScene)
+    ViewerFrameLifecycle::ViewerFrameLifecycle(osgViewer::Viewer& viewer)
         : mViewer(viewer)
-        , mSynchronizeScene(std::move(synchronizeScene))
     {
     }
 
@@ -24,12 +20,6 @@ namespace MWRender
     bool ViewerFrameLifecycle::renderFrame(const Render::SceneSubmission& /*submission*/)
     {
         return renderFrame();
-    }
-
-    void ViewerFrameLifecycle::synchronizeScene(Render::SceneData& sceneData)
-    {
-        if (mSynchronizeScene)
-            mSynchronizeScene(sceneData);
     }
 
     void ViewerFrameLifecycle::advanceFrame(double simulationTime)
--- apps/openmw/mwrender/viewerframelifecycle.hpp
+++ apps/openmw/mwrender/viewerframelifecycle.hpp
@@ -1,8 +1,6 @@
 #ifndef OPENMW_MWRENDER_VIEWER_FRAME_LIFECYCLE_H
 #define OPENMW_MWRENDER_VIEWER_FRAME_LIFECYCLE_H
 
-#include <functional>
-
 #include <components/render/frame.hpp>
 
 namespace osgViewer
@@ -17,18 +15,16 @@ namespace MWRender
     class ViewerFrameLifecycle final : public Render::FrameLifecycle
     {
     public:
-        explicit ViewerFrameLifecycle(osgViewer::Viewer& viewer,
-            std::function<void(Render::SceneData&)> synchronizeScene = {});
+        explicit ViewerFrameLifecycle(osgViewer::Viewer& viewer);
 
         bool renderFrame() override;
         bool renderFrame(const Render::SceneSubmission& submission) override;
         bool consumesSceneSubmission() const override { return false; }
-        void synchronizeScene(Render::SceneData& sceneData) override;
+        void synchronizeScene(Render::SceneData& /*sceneData*/) override {}
         void advanceFrame(double simulationTime) override;
 
     private:
         osgViewer::Viewer& mViewer;
-        std::function<void(Render::SceneData&)> mSynchronizeScene;
     };
 }
 
