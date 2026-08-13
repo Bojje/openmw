264ac3e529342ad9710b3972dd483c1529d22a31
Mathias <m-boisen@hotmail.com>
2026-08-13 10:05:45 +0200

    Drop unused neutral animation include

--- components/nif/meshconverter.cpp
+++ components/nif/meshconverter.cpp
@@ -19,7 +19,6 @@
 #include "texture.hpp"
 #include <components/render/meshconversion.hpp>
 #include <components/render/math.hpp>
-#include <components/render/animationmask.hpp>
 #include <components/vfs/pathutil.hpp>
 #include <components/misc/strings/lower.hpp>
 
